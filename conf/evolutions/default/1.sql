a6d34a99f7162b9fe3dfd43030c570900722cfe7
Sondre Eikanger Kvalø <zapodot@gmail.com>
2013-08-25 13:35:34 +0200

    set production folder prefix

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -7,12 +7,13 @@ create table resource (
   constraint pk_resource primary key (id))
 ;
 
-INSERT INTO resource (title, url) VALUES ('Play2 documentation', 'http://www.playframework.com/documentation/2.1.x/Home');
+INSERT INTO resource (title, url) VALUES ('Play Framwork 2.1.X docs', 'http://www.playframework.com/documentation/2.1.x/Home');
 INSERT INTO resource (title, url) VALUES ('RequireJS', 'http://requirejs.org/');
 INSERT INTO resource (title, url) VALUES ('LESS', 'http://lesscss.org/');
 INSERT INTO resource (title, url) VALUES ('Coffeescript', 'http://coffeescript.org/');
 INSERT INTO resource (title, url) VALUES ('Bootstrap', 'http://getbootstrap.com/');
 INSERT INTO resource (title, url) VALUES ('EBean ORM', 'http://www.avaje.org/ebean/documentation.html');
+INSERT INTO resource (title, url) VALUES ('Play2 Modules directory', 'http://www.playframework.com/documentation/2.1.x/Modules');
 
 
 
